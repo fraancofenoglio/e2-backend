00d703f6c25b2a46e91748363ab39f281ff8e081
fraancofenoglio <francofeno26@gmail.com>
2022-12-17 21:44:17 -0300

    todo limpio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
-import employeesRoutes from "./routes/gastos.routes.js";
+import RutaDeGastos from "./routes/gastos.routes.js";
 
 const app = express();
 
 app.use(express.json())
 
-app.use(employeesRoutes)
+app.use(RutaDeGastos)
 
 app.use((req, res, next) => {
     res.status(404).json({
